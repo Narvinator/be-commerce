0c6f7a11bbe008b38bccf77b9c1725f89fa371ba
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-12-03 00:33:12 -0500

    filled the init with db instruction

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-   
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
