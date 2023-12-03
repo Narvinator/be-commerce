423f1dbefc5ef7bcce7111e926a6a3b4f9bc7a0c
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-12-03 01:50:32 -0500

    Added foreign keys to the index.js

--- models/index.js
+++ models/index.js
@@ -8,7 +8,19 @@ Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
+Product.belongsToMany(Tag, { 
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
+
+Tag.belongsToMany(Product, { 
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
